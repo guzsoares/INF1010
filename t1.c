3291de9d1ad41168c6d1f75e5b307a40b1ffcabd
Gustavo Molina Soares <97996136+guzsoares@users.noreply.github.com>
2022-09-01 12:15:10 -0300

    acabou

--- t1.c
+++ t1.c
@@ -47,24 +47,99 @@ typedef struct lista Lista;
 
 /* --------------------------------------------------------------------------------- */
 
-/* Funcoes para calcular as áreas */
+/* calcular as áreas */
 
-static float area_ret(Retangulo *r){
+float areaRetang(Retangulo *r);
+float areaTriang(Triangulo *t);
+float areaCirc(Circulo *c);
+float areaHexa(Hexagono *h);
+float areaQuad(Quadrado *q);
+float calculaArea(Lista *dados);
+float maiorArea(Lista *dados);
+
+/* ----------------------------------------------------------------------- */
+
+/* calcular perimetro */
+
+float perimRetang(Retangulo *r);
+float perimTriang(Triangulo *t);
+float perimCirc(Circulo *c);
+float perimHexa(Hexagono *h);
+float perimQuad(Quadrado *q);
+float calculaPerim(Lista *dados);
+float maiorPerim(Lista *dados);
+
+/* ---------------------------------------------------------------------------- */
+
+/* cria as formas nas listas */
+
+Lista* criaRetangulo(float b, float h, Lista* dados);
+Lista* criaCirculo(float r, Lista* dados);
+Lista* criaTriangulo(float b, Lista *dados);
+Lista* criaHexagono(float b, Lista* dados);
+Lista* criaQuadrado(float b, Lista *dados);
+
+/* ------------------------------------------------------------------------------ */
+
+/* manipulacao de lista */
+
+void imprimeLista(Lista* dados);
+Lista* buscaIndex (Lista* dados, int val);
+Lista* retiraLista(Lista* dados, int index);
+void freeLista(Lista* dados);
+
+/* ----------------------------------------------------------------------------------------------- */
+
+int main (int argc, char **argv){
+    Lista *dados = NULL;
+    dados = criaTriangulo(3.0, dados);
+    dados = criaQuadrado(2.0, dados);
+    dados = criaRetangulo(3.0, 5.0, dados);
+    dados = criaTriangulo(5.0, dados);
+    dados = criaCirculo(4.0, dados);
+    dados = criaHexagono(3.0, dados);
+    dados = criaTriangulo(4.0, dados);
+
+    puts("Lista inicial:\n");
+
+    imprimeLista(dados);
+
+    puts("");
+
+    printf("Maior área: %.2f\n",maiorArea(dados));
+
+    printf("Maior perimetro: %.2f\n",maiorPerim(dados));
+
+    dados = retiraLista(dados,6);
+
+    puts("");
+
+    puts("Lista após remoção:\n");
+
+    imprimeLista(dados);
+
+    
+    return 0;
+}
+
+// FUNÇÕES
+
+float areaRetang(Retangulo *r){
     return(r->b*r->h);
 }
-static float area_trig(Triangulo *t){
+float areaTriang(Triangulo *t){
     float area = (sqrt(3)/4)*pow(t->b,2);
     return (area);
 }
-static float area_circ(Circulo *c){
+float areaCirc(Circulo *c){
     float area = M_PI*pow(c->r,2);
     return area;
 }
-static float area_hex(Hexagono *h){
+float areaHexa(Hexagono *h){
     float area = (3*sqrt(3)*pow(h->b,2))/2;
     return area;
 }
-static float area_quad(Quadrado *q){
+float areaQuad(Quadrado *q){
     return (q->b * q->b);
 }
 
@@ -73,23 +148,23 @@ float calculaArea(Lista *dados){
 
     switch (dados->tipo){
         case QUA:
-            area  = area_quad((Quadrado*)dados->info);
+            area  = areaQuad((Quadrado*)dados->info);
             break;
 
         case TRI:
-            area  = area_trig((Triangulo*)dados->info);        
+            area  = areaTriang((Triangulo*)dados->info);        
             break;
 
         case HEX:
-            area  = area_hex((Hexagono*)dados->info);
+            area  = areaHexa((Hexagono*)dados->info);
             break;
 
         case RET:
-            area  = area_ret((Retangulo*)dados->info);
+            area  = areaRetang((Retangulo*)dados->info);
             break;          
 
         case CIR:
-            area  = area_circ((Circulo*)dados->info);
+            area  = areaCirc((Circulo*)dados->info);
             break;
 
         default:
@@ -112,9 +187,65 @@ float maiorArea(Lista *dados){
     return maiorArea;
 }
 
-/* ----------------------------------------------------------------------- */
+float perimRetang(Retangulo *r){
+    return (r->b *2 + r->h *2);
+}
+float perimTriang(Triangulo *t){
+    return (t->b * 3);
+}
+float perimCirc(Circulo *c){
+    return (c->r * 2 * M_PI);
+}
+float perimHexa(Hexagono *h){
+    return (h->b * 6);
+}
+float perimQuad(Quadrado *q){
+    return (q->b * 4);
+}
 
-/* cria as formas nas listas */
+float calculaPerim(Lista *dados){
+    float perim;
+
+    switch (dados->tipo){
+        case QUA:
+            perim  = perimQuad((Quadrado*)dados->info);
+            break;
+
+        case TRI:
+            perim  = perimTriang((Triangulo*)dados->info);        
+            break;
+
+        case HEX:
+            perim  = perimHexa((Hexagono*)dados->info);
+            break;
+
+        case RET:
+            perim  = perimRetang((Retangulo*)dados->info);
+            break;          
+
+        case CIR:
+            perim  = perimCirc((Circulo*)dados->info);
+            break;
+
+        default:
+            printf("Tipo inexistente");
+            break;  
+    }
+
+    return perim;
+}
+
+float maiorPerim(Lista *dados){
+    float maiorPerim = 0;
+    Lista *aux;
+    for(aux = dados; aux != NULL; aux = aux->prox){
+        float area = calculaPerim(aux);
+        if(area > maiorPerim)
+            maiorPerim = area;
+    }
+
+    return maiorPerim;
+}
 
 Lista* criaRetangulo(float b, float h, Lista* dados){
     Retangulo* ret;
@@ -165,7 +296,7 @@ Lista* criaCirculo(float r, Lista* dados){
     return dados;
 }
 
-Lista* cria_triangulo(float b, Lista *dados){
+Lista* criaTriangulo(float b, Lista *dados){
     Triangulo* trig;
     Lista* curr_data = NULL;
 
@@ -183,7 +314,7 @@ Lista* cria_triangulo(float b, Lista *dados){
         return dados;
     }
 
-    dados->prox = cria_triangulo(b, dados->prox);
+    dados->prox = criaTriangulo(b, dados->prox);
 
 
     return dados;
@@ -237,28 +368,24 @@ Lista* criaQuadrado(float b, Lista *dados){
     return dados;
 }
 
-/* ------------------------------------------------------------------------------ */
-
-/* manipulacao de lista */
-
 void imprimeLista(Lista* dados){
     Lista *aux;
     for(aux = dados; aux !=NULL; aux = aux->prox){
         switch (aux->tipo){
             case QUA:
-                printf("Quadrado area %.2f \n",calculaArea(aux));
+                printf("Quadrado de area: %.2f e de perimetro: %.2f  \n",calculaArea(aux), calculaPerim(aux));
                 break;
             case TRI:
-                printf("Triangulo de area %.2f \n", calculaArea(aux));      
+                printf("Triangulo de area: %.2f e de perimetro: %.2f  \n", calculaArea(aux), calculaPerim(aux));      
                 break;
             case HEX:
-                printf("Hexagono area %.2f \n",calculaArea(aux)); 
+                printf("Hexagono de area: %.2f e de perimetro: %.2f \n",calculaArea(aux), calculaPerim(aux)); 
                 break;
             case RET:
-                printf("Retangulo  area %.2f \n",calculaArea(aux));           
+                printf("Retangulo de area: %.2f e de perimetro: %.2f  \n",calculaArea(aux), calculaPerim(aux));           
                 break;
             case CIR:
-                printf("Circulo de area %.2f \n", calculaArea(aux));           
+                printf("Circulo de area: %.2f e de perimetro: %.2f  \n", calculaArea(aux), calculaPerim(aux));           
                 break;
             default:
                 printf("Tipo inexistente");
@@ -311,30 +438,4 @@ void freeLista(Lista* dados){
         free(aux);  // liberando posição da lista
         aux = next; // resetando auxiliar
     }
-}
-
-/* ----------------------------------------------------------------------------------------------- */
-
-int main (int argc, char **argv){
-    Lista *dados = NULL;
-    dados = cria_triangulo(3.0, dados);
-    dados = cria_triangulo(4.0, dados);
-    dados = cria_triangulo(5.0, dados);
-    dados = criaQuadrado(2.0, dados);
-    dados = criaRetangulo(3.0, 5.0, dados);
-    dados = criaCirculo(4.0, dados);
-    dados = criaHexagono(3.0, dados);
-
-    imprimeLista(dados);
-
-    printf("Maior área: %.2f\n",maiorArea(dados));
-
-    dados = retiraLista(dados,6);
-
-    puts("");
-
-    imprimeLista(dados);
-
-    
-    return 0;
 }
