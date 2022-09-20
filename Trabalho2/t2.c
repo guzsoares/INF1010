4a7699796d5a41c4f53f4e407df211b19a4e7277
Gustavo Molina Soares <97996136+guzsoares@users.noreply.github.com>
2022-09-20 19:24:20 -0300

    algumas questoes

--- Trabalho2/t2.c
+++ Trabalho2/t2.c
@@ -11,22 +11,72 @@ Node *insereABB(int valor, Node *arvore);
 Node* retiraABB(Node* r, int num);
 void insereAB(Node *pai, Node *filho);
 Node *criaAB(int num);
-void print2DUtil(Node *root, int space);
-void print2D(Node *root);
+int alturaArvore(Node * root);
+void printAltura(Node * root, int level);
+void printArvoreNivel(Node * root);
+Node *criaArvoreExemplo(int valor, Node *arvore, int isRight);
+void isBinary(Node *arvore);
+int isBinaryLogic(Node *arvore);
 
 int main(void){
     Node *abb = NULL;
+    Node *exemplo = NULL;
+
+    //questao 1
+    exemplo = criaArvoreExemplo(10, exemplo, 0);
+    exemplo = criaArvoreExemplo(5, exemplo, 0);
+    exemplo = criaArvoreExemplo(13, exemplo, 1);
+    exemplo = criaArvoreExemplo(15, exemplo, 1);
+    exemplo = criaArvoreExemplo(7, exemplo, 0);
+    exemplo = criaArvoreExemplo(1, exemplo, 0);
 
     //questao 2
-    insereABB(10, abb);
-    insereABB(5, abb);
-    insereABB(7, abb);
-    insereABB(1, abb);
-    insereABB(13, abb);
-    insereABB(15, abb);
+    abb = insereABB(10, abb);
+    abb = insereABB(5, abb);
+    abb = insereABB(7, abb);
+    abb = insereABB(1, abb);
+    abb = insereABB(13, abb);
+    abb = insereABB(15, abb);
 
-    printf("\n");
-    print2D(abb);
+
+    printf("questao 4\n");
+    puts("");
+    printf("Arvore 1 em nível\n");
+
+    printArvoreNivel(exemplo);
+
+    printf("Arvore 2 em nível\n");
+
+    printArvoreNivel(abb);
+
+    printf("questao 5\n");
+    puts("");
+    isBinary(exemplo);
+    isBinary(abb);
+
+    printf("questao 6\n");
+    puts("");
+    int x = alturaArvore(exemplo);
+    printf("%d\n",x);
+
+    printf("questao 8\n");
+    puts("");
+    abb = retiraABB(abb, 1);
+    printf("Primeira exclusão\n");
+
+    printArvoreNivel(abb);
+
+    abb = retiraABB(abb, 13);
+    printf("Segunda exclusão\n");
+
+    printArvoreNivel(abb);
+
+    abb = retiraABB(abb, 10);
+    printf("Terceira exclusão\n");
+
+    printArvoreNivel(abb);
+
+
     return 0;
 }
 
@@ -48,6 +98,27 @@ Node *insereABB(int valor, Node *arvore){
     }
     return arvore;
 }
+
+Node *criaArvoreExemplo(int valor, Node *arvore, int isRight){
+    Node *aux;
+    if(arvore == NULL){
+        aux =(Node*)malloc(sizeof(Node)); //inicializa a arvore
+        aux->direita = NULL;
+        aux->esquerda = NULL;
+        aux->central = valor; //primeiro valor
+        arvore = aux; //inicializa a arvore, para continuar o loop
+    } else {
+        if (isRight == 1){
+        arvore->direita = criaArvoreExemplo(valor, arvore->direita, 0);
+        }
+        else {
+        arvore->esquerda = criaArvoreExemplo(valor, arvore->esquerda, 1);
+        }
+    }
+
+    return arvore;
+}
+
 //Essa funcao retira um no escolhido de maneira recursiva (ABB)
 Node* retiraABB(Node* r, int num){
     if (r == NULL)
@@ -86,22 +157,73 @@ void insereAB(Node *pai, Node *filho){
     pai->esquerda = filho;
 }
 
-void print2DUtil(Node *root, int space){
-    // Base case
-    if (root == NULL)
+void printArvoreNivel(Node * root){
+    if (root == NULL){
         return;
-    space += 10;
-    print2DUtil(root->direita, space);
- 
-    printf("\n");
-    for (int i = 10; i < space; i++)
-        printf(" ");
-    printf("%d\n", root->central);
- 
-   print2DUtil(root->esquerda, space);
-}
- 
+    }
 
-void print2D(Node *root){
-   print2DUtil(root, 0);
+    int h = alturaArvore(root);
+    int i;
+    for (i = 0; i <= h; i++){
+        printAltura(root, i);
+        printf("\n");
+    }
+}
+
+void printAltura(Node * root, int level){
+
+    if (root == NULL){
+        return;
+    }
+    if (level == 1){
+        printf("%d ", root->central);
+    }
+    else if (level > 1) {
+        printAltura(root->esquerda, level - 1);
+        printAltura(root->direita, level - 1);
+    }
+}
+
+int alturaArvore(Node * root){
+    if (root == NULL){
+        return 0;
+    }
+
+    int leftHeight = alturaArvore(root->esquerda);
+    int rightHeight = alturaArvore(root->direita);
+    if (leftHeight > rightHeight){
+        return leftHeight + 1;
+    }
+    if (rightHeight > leftHeight){
+        return rightHeight + 1;
+    }
+    return leftHeight + 1;
+}
+
+
+int isBinaryLogic(Node* arvore)
+{
+    if (arvore == NULL)
+        return 1;
+ 
+    if (arvore->esquerda != NULL && arvore->esquerda->central > arvore->central)
+        return 0;
+ 
+    if (arvore->direita != NULL && arvore->direita->central < arvore->central)
+        return 0;
+ 
+    if (!isBinaryLogic(arvore->esquerda) || !isBinaryLogic(arvore->direita))
+        return 0;
+ 
+    return 1;
+}
+
+void isBinary(Node *arvore){
+    int value = isBinaryLogic(arvore);
+    if (value  == 1){
+        printf("É binária\n");
+    } 
+    else {
+        printf("Não é binária\n");
+    }
 }
