c62853f6cf0243c34998ed066eb1bb9a5e6fa584
Leo Lomardo <leoland771@gmail.com>
2022-10-14 18:26:27 -0300

    Update main.c

--- Trabalho3/main.c
+++ Trabalho3/main.c
@@ -2,24 +2,31 @@
 #include <stdlib.h>
 
 typedef struct heap {
-    int max; 
+    int tamanho; 
     int pos; 
     int* prioridade; 
 }Heap;
 
 
-Heap* heap_cria(int max);
+Heap* heap_cria(int tamanho);
 
 
 
-Heap* heap_cria(int max){
+Heap* heap_cria(int tamanho){
     Heap* heap=(Heap*)malloc(sizeof(Heap));
-    heap->max=max;
+    heap->tamanho=tamanho;
     heap->pos=0;
-    heap->prioridade=(int *)malloc(max*sizeof(int));
+    heap->prioridade=(int *)malloc(tamanho*sizeof(int));
     return heap;
 }
-
+void print(Heap *h){
+    int i;
+    printf("____________Print Heap_____________\n");
+    for(i=0;i< h->tamanho;i++){
+        printf("-> %d ",h->prioridade[i]);
+    }
+    printf("->__/\\__\n");
+}
 
 int main (void){
 
