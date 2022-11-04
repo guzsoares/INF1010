ef07e408257fc298520563120c0acf4d36d90e45
Leo Lomardo <leoland771@gmail.com>
2022-11-04 18:39:18 -0300

    fim

--- Trabalho4/hash.c
+++ Trabalho4/hash.c
@@ -9,15 +9,16 @@
 #define CPF_SIZE 5     /*  nao conta com o "\n"  numero de digitos = 11 numero de caracteres = 14(contando com . e - )*/
 
 /*__________________________________________________________________________*/
-void inicializaTabela(long *tabelaHash);
-int mandaChave(long cpf, int k);
-int buscaElemento(long *tabelaHash, long valor);
+void inicializaTabela(unsigned long *tabelaHash);
+int mandaChave(unsigned long cpf, int k);
+int buscaElemento(unsigned long *tabelaHash, unsigned long valor);
+int achaMax(int *numColisoes_Y, int posicao);
 /*__________________________________________________________________________*/
 
 int main(void){
-    long tabelaHash[HASH_SIZE];
+    unsigned long tabelaHash[HASH_SIZE];
     long auxLeitura = 0;
-    int chave, numLeituras = 0, numColisoes = 0;
+    int chave, numLeituras = 1, numColisoes = 0;
     int numLeituras_X[HASH_SIZE]; //vetores para plotar grafico
     int numColisoes_Y[HASH_SIZE];
     int numColisoesTotal = 0;
@@ -25,7 +26,7 @@ int main(void){
     FILE *arquivo_graph = fopen("grafico.txt", "w");
 
     int resultadoBusca = 0;
-    long valorBusca = 12345678901;
+    unsigned long valorBusca = 44840341826;
 
     if(!arquivo_entrada){
         printf("Erro ao abrir o arquivo.\n");
@@ -60,49 +61,66 @@ int main(void){
     }
 
     //Monta o vetor p plotar grafico
-    for(int z = 1; z < HASH_SIZE; z++){
+    int z = 1;
+    for(z = 1; z < HASH_SIZE; z++){
         fprintf(arquivo_graph,"%d\t %d\n",numLeituras_X[z],numColisoes_Y[z]);
     }
 
-
+    
     resultadoBusca = buscaElemento(tabelaHash, valorBusca);
+
     if(resultadoBusca == -3){
         printf("Elemento nao encontrado\n");
     }else{
         printf("O valor %ld foi encontrado e esta na posicao %d ta tabela\n", valorBusca, resultadoBusca);
     }
-
+    int maxColisoes = achaMax(numColisoes_Y, z);
+    printf("O numero maximo de colisoes foi %d", maxColisoes);
     fclose(arquivo_graph);
     fclose(arquivo_entrada);
     return 0;
 }
 
 //funcao coloca tudo com -1 pra facilitar verificacao se ta vazio ou n, ja q -1 nao e um valor possivel
-void inicializaTabela(long *tabelaHash){
-    for(int i = 1; i< HASH_SIZE; i++){
+void inicializaTabela(unsigned long *tabelaHash){
+    for(int i = 0; i< HASH_SIZE; i++){
         tabelaHash[i] = -1;
     }
 }
-int buscaElemento(long *tabelaHash, long valor){
+
+int buscaElemento(unsigned long *tabelaHash, unsigned long valor){
     int posicao, k = 0;
 
     posicao = mandaChave(valor, k);
     while(tabelaHash[posicao] != valor){
-        posicao = mandaChave(valor, k++);
+        posicao = mandaChave(valor, k+=1);
+
         if(tabelaHash[posicao] == valor){
             return posicao;
         }
         if((posicao+k) > HASH_SIZE){
             break;
+            return -3;
         }
     }
-    
-    return -3;
+    return posicao;
 }
-int mandaChave(long cpf, int k){
+
+int mandaChave(unsigned long cpf, int k){
     int chave = 0;
 
     chave = (cpf + k) % DATA_SIZE;
 
     return chave;
+}
+int achaMax(int *numColisoes_Y, int posicao){
+    int maxColisoes = -1;
+    for(int i = 0; i <1000; i++){
+        if(numColisoes_Y[i] > maxColisoes){
+            maxColisoes = numColisoes_Y[i];
+            posicao = i;
+        }
+    }
+
+    return maxColisoes;
 }
