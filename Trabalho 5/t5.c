75de4e48fb86c17d88bcd163915b5a176c814051
Leo Lomardo <leoland771@gmail.com>
2022-11-28 19:51:18 -0300

    MST Kruskal implementation

--- Trabalho 5/t5.c
+++ Trabalho 5/t5.c
@@ -5,6 +5,8 @@
 
 #define numNodes 9
 
+
+
 typedef struct vertex{
     int value;
     int cost;
@@ -16,6 +18,53 @@ typedef struct graph{
     Vertex **adjList;
 }Graph;
 
+typedef struct Edge{
+  int src;
+  int dest;
+  int cost;
+  struct Edge *next;
+}Edge;
+Edge *createEdgeList(Graph *grafo);
+Edge* insertEdge(Edge* conectionList, int src, int dest, int cost);
+Edge* createEdge(int src, int dest, int cost);
+Graph *createGraph(int numVertex);
+Vertex *createVertex(int value);
+void printGraph(Graph* graph);
+void addConnections(Graph *graph, int s, int d, int cost);
+void kruskal(Edge* conectionList, Graph *kruskalTree);
+void djistrikaPath(int startNode, Graph *graph, int endNode);
+void Union(int vRep[], int x, int y);
+int find(int vRep[], int i);
+int minDistance(int *dist, bool *isPath);
+void sentNegative(int *vRep, int numVertice);
+
+int main(){
+  Edge *conectionList;
+  Graph* graph = createGraph(numNodes);
+  Graph* kruskalTree = createGraph(numNodes);
+  addConnections(graph, 0, 1, 6);
+  addConnections(graph, 0, 2, 9);
+  addConnections(graph, 0, 3, 15);
+  addConnections(graph, 1, 4, 20);
+  addConnections(graph, 4, 3, 4);
+  addConnections(graph, 4, 6, 24);
+  addConnections(graph, 3, 6, 7);
+  addConnections(graph, 3, 5, 10);
+  addConnections(graph, 5, 2, 13);
+  addConnections(graph, 2, 8, 28);
+  addConnections(graph, 5, 6, 5);
+  addConnections(graph, 6, 7, 15);
+  addConnections(graph, 7, 8, 17);
+
+  djistrikaPath(0, graph, 7);
+
+  conectionList = createEdgeList(graph);
+  kruskal(conectionList, kruskalTree);
+
+  printGraph(kruskalTree);
+
+  return 0;
+}
 
 Vertex *createVertex(int value){
     Vertex *newVertex = malloc(sizeof(Vertex));
@@ -110,27 +159,93 @@ void djistrikaPath(int startNode, Graph *graph, int endNode){
 
     }
   }
-
-  printf("The shortest path between the starting node %d and the ending node %d is %d", startNode, endNode, dist[endNode]);
+  printf("The shortest path between the starting node %d and the ending node %d is %d\n", startNode, endNode, dist[endNode]);
 }
 
-int main() {
-  Graph* graph = createGraph(numNodes);
-  addConnections(graph, 0, 1, 6);
-  addConnections(graph, 0, 2, 9);
-  addConnections(graph, 0, 3, 15);
-  addConnections(graph, 1, 4, 20);
-  addConnections(graph, 4, 3, 4);
-  addConnections(graph, 4, 6, 24);
-  addConnections(graph, 3, 6, 7);
-  addConnections(graph, 3, 5, 10);
-  addConnections(graph, 5, 2, 13);
-  addConnections(graph, 2, 8, 28);
-  addConnections(graph, 5, 6, 5);
-  addConnections(graph, 6, 7, 15);
-  addConnections(graph, 7, 8, 17);
+void kruskal(Edge* conectionList, Graph *kruskalTree){
+  Edge *auxEdge = conectionList;
+  int vRep[numNodes];
+  int x, y;
 
-  djistrikaPath(0, graph, 7);
+  sentNegative(vRep, kruskalTree->num_vertex);
 
-  return 0;
+  while(auxEdge){
+    x = find(vRep, auxEdge->src);
+    y = find(vRep, auxEdge->dest);	
+
+    if (x != y){
+      Union(vRep, x, y);
+      addConnections(kruskalTree, auxEdge->src, auxEdge->dest, auxEdge->cost);
+    }
+    auxEdge = auxEdge->next;
+  }
+}
+
+void sentNegative(int *vRep, int numVertice){
+  for(int i = 0; i <numVertice; i++){
+    vRep[i] = -1;
+  }
+}
+
+int find(int vRep[], int i){
+  if (vRep[i] == -1){
+    return i;
+  }
+  return find(vRep, vRep[i]);
+}
+
+void Union(int vRep[], int x, int y){
+  vRep[x] = y;
+}
+
+Edge *createEdgeList(Graph *grafo){
+  Edge* conectionList = NULL;
+
+  for(int i = 0; i < grafo->num_vertex; i++){
+    for(Vertex* aux = grafo->adjList[i]; aux != NULL; aux = aux->next){
+      conectionList = insertEdge(conectionList, i, aux->value, aux->cost);
+    }
+  }
+  return conectionList;
+}
+
+Edge* insertEdge(Edge* conectionList, int src, int dest, int cost){
+  Edge *newEdge = createEdge(src, dest, cost);
+  Edge* anterior = NULL;
+  Edge* atual = conectionList;
+
+  if(conectionList == NULL){
+    return newEdge;
+  }
+
+  while(atual != NULL && cost >= atual->cost){
+    if(cost == atual->cost && (src == atual->dest && dest == atual->src)){
+      free(newEdge);			
+      return conectionList;		
+    }
+    anterior = atual;		
+    atual = atual->next;
+  }
+  newEdge->next = atual;		
+
+  if(anterior == NULL){
+    return newEdge;			
+  }
+  anterior->next = newEdge;	
+  
+  return conectionList;			
+}
+
+Edge* createEdge(int src, int dest, int cost){
+  Edge* newEdge = (Edge *) malloc(sizeof(Edge));
+  if(newEdge == NULL){
+	  printf("Erro ao alocar memoria\n");
+	  exit(1);
+  }
+  newEdge->src = src;		
+  newEdge->dest = dest;	
+  newEdge->cost = cost;	
+  newEdge->next = NULL;	
+
+  return newEdge;
 }
